089c6f9311430f9787070a9e4c3bed4ef27b8515
Shayne Koestler <shayne.koestler@kineticdata.com>
2017-11-27 17:35:24 -0600

    updated to proxy websocket connections, except the websocket used for hmr

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -90,7 +90,7 @@ choosePort(HOST, DEFAULT_PORT)
     // Hardcode the proxy settings (with a couple of configurable properties
     // that can be defined in the config.js file).
     const kinopsProxySetting = {
-      '/': {
+      '^((?!sockjs-node/\\d{3}/\\w{8}/websocket).)*$': {
         target: kinopsConfig.kineticWebserver,
         headers: {
           'X-Webpack-Bundle-Name': kinopsConfig.bundleName,
@@ -98,6 +98,7 @@ choosePort(HOST, DEFAULT_PORT)
         secure: false,
         autoRewrite: true,
         protocolRewrite: 'http',
+        ws: true,
       },
     };
     const proxyConfig = prepareProxy(kinopsProxySetting, paths.appPublic);
